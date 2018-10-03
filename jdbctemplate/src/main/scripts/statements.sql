968ef5a4febd05d518d0245f51c89c126200a54a
Manfred Quasten <manfred.quasten@arcor.de>
2018-10-03 16:47:40 +0200

    no message

---
+++ jdbctemplate/src/main/scripts/statements.sql
@@ -0,0 +1,14 @@
+CREATE TABLE artist (
+  id bigint NOT NULL,
+  firstname varchar(250) not null,
+  lastname varchar(250) not null,
+  hotscore int not null,
+  PRIMARY KEY( id )
+  )
+
+
+  drop table artist
+
+  select * from artist
+
+  truncate table artist
