9ca4d08003049349c6914e5e3dda0a9e9aacc9c5
Manfred Quasten <manfred.quasten@arcor.de>
2018-10-14 14:29:40 +0200

    finish test

--- jdbctemplate/src/test/java/de/mq/jdbc/util/persistence/UnitOfWorkTest.java
+++ jdbctemplate/src/test/java/de/mq/jdbc/util/persistence/UnitOfWorkTest.java
@@ -1,6 +1,7 @@
 package de.mq.jdbc.util.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -86,7 +87,7 @@ class UnitOfWorkTest {
 	    final SqlParameterSource[]   sqlParameterSourceArtsist = persistenceInfoArtsists.getSqlParameterSource().get();
 	    assertEquals(10, sqlParameterSourceArtsist.length);
 	    
-	    IntStream.range(0, 10).forEach(i -> assertArtistSource(sqlParameterSourceArtsist, i));
+	    IntStream.range(0, 10).forEach(i -> assertArtistSource(sqlParameterSourceArtsist, i, 0));
 	    
 	    final PersistenceInfo persistenceInfoVideos =  persistenceInfos.get(1);
 	    assertEquals(Video.class.getAnnotation(Persistence.class).value(), persistenceInfoVideos.getInsertSql());
@@ -95,7 +96,7 @@ class UnitOfWorkTest {
 	    final SqlParameterSource[]   sqlParameterSourceVideo = persistenceInfoVideos.getSqlParameterSource().get();
 	    assertEquals(10, sqlParameterSourceVideo.length);
 	    
-	    IntStream.range(0, 10).forEach(i -> assertVideoSource(sqlParameterSourceVideo, i));
+	    IntStream.range(0, 10).forEach(i -> assertVideoSource(sqlParameterSourceVideo, i, 0));
 	    
 	    
 	    final PersistenceInfo persistenceInfoAwards =  persistenceInfos.get(2);
@@ -105,26 +106,68 @@ class UnitOfWorkTest {
 	    final SqlParameterSource[]   sqlParameterSourceAward = persistenceInfoAwards.getSqlParameterSource().get();
 	    assertEquals(10, sqlParameterSourceAward.length);
 	    
-	    IntStream.range(0, 10).forEach(i -> assertAwardSource(sqlParameterSourceAward, i));
+	    IntStream.range(0, 10).forEach(i -> assertAwardSource(sqlParameterSourceAward, i,0));
 		
 	}
 
 	
 
-	private void assertArtistSource(final SqlParameterSource[] sqlParameterSourceArtsist, final int i) {
-		assertEquals(ARTISTNAME_PREFIX + (1 +i) , sqlParameterSourceArtsist[i].getValue(LASTNAME_FIELDNAME));
-		assertEquals( Long.valueOf(1+i),  sqlParameterSourceArtsist[i].getValue(ID_FIELDNAME));
+	private void assertArtistSource(final SqlParameterSource[] sqlParameterSourceArtsist, final int i, final int offset) {
+		assertEquals(ARTISTNAME_PREFIX + (offset + 1 +i) , sqlParameterSourceArtsist[i].getValue(LASTNAME_FIELDNAME));
+		assertEquals( Long.valueOf(offset+ 1+i),  sqlParameterSourceArtsist[i].getValue(ID_FIELDNAME));
 	}
 	
-	private void assertVideoSource(final SqlParameterSource[] sqlParameterSourceVideo, final int i) {
-		assertEquals(VIDEO_NAME_PREFIX + (1 +i) , sqlParameterSourceVideo[i].getValue(NAME_FIELDNAME));
-		assertEquals( Long.valueOf(1+i),  sqlParameterSourceVideo[i].getValue(ID_FIELDNAME));
+	private void assertVideoSource(final SqlParameterSource[] sqlParameterSourceVideo, final int i, final int offset) {
+		assertEquals(VIDEO_NAME_PREFIX + (offset+ 1 +i) , sqlParameterSourceVideo[i].getValue(NAME_FIELDNAME));
+		assertEquals( Long.valueOf(offset+1+i),  sqlParameterSourceVideo[i].getValue(ID_FIELDNAME));
 	
 	}
 	
-	private void assertAwardSource(final SqlParameterSource[] sqlParameterSourceAward, final int i) {
-		assertEquals(AWARD_NAME_PREFIX + (1 +i) , sqlParameterSourceAward[i].getValue(NAME_FIELDNAME));
-		assertEquals( Long.valueOf(1+i),  sqlParameterSourceAward[i].getValue(ID_FIELDNAME));
+	private void assertAwardSource(final SqlParameterSource[] sqlParameterSourceAward, final int i, final int offset) {
+		assertEquals(AWARD_NAME_PREFIX + (offset+ 1 +i) , sqlParameterSourceAward[i].getValue(NAME_FIELDNAME));
+		assertEquals( Long.valueOf(offset+1+i),  sqlParameterSourceAward[i].getValue(ID_FIELDNAME));
 	}
+	
+	@Test
+	void getAndClear() {
+	
+		assertEquals(12, batchUpdates.size());
+		
+		final Optional<Entry<List<Long>, List<PersistenceInfo>>>  results = unitOfWork.getAndClear();
+		
+		assertTrue(results.isPresent());
+		
+		assertEquals(2, results.get().getKey().size());
+		
+		assertEquals(3, results.get().getValue().size());
+		
+		assertEquals(Arrays.asList(11L, 12L), results.get().getKey());
+		
+		
+		
+	    final List<PersistenceInfo>  persistenceInfos = results.get().getValue();
+		    final PersistenceInfo persistenceInfoArtsists =  persistenceInfos.get(0);
+		    assertEquals(Artist.class.getAnnotation(Persistence.class).value(), persistenceInfoArtsists.getInsertSql());
+		    assertEquals(0, persistenceInfoArtsists.getOrder());
+		    
+		    final SqlParameterSource[]   sqlParameterSourceArtsist = persistenceInfoArtsists.getSqlParameterSource().get();
+		    assertEquals(2, sqlParameterSourceArtsist.length);
+		    
+		    IntStream.range(0, 2).forEach(i -> assertArtistSource(sqlParameterSourceArtsist, i, 10 ));
+		    
+		    
+		    
+		    final PersistenceInfo persistenceInfoVideos =  persistenceInfos.get(1);
+		    final SqlParameterSource[]   sqlParameterSourceVideo = persistenceInfoVideos.getSqlParameterSource().get();
+		    assertEquals(2, sqlParameterSourceVideo.length);
+		    IntStream.range(0, 2).forEach(i -> assertVideoSource(sqlParameterSourceVideo, i, 10 ));
+		    
+		    
+		    final PersistenceInfo persistenceInfoAwards =  persistenceInfos.get(2);
+		    final SqlParameterSource[]   sqlParameterSourceAwards = persistenceInfoAwards.getSqlParameterSource().get();
+		    assertEquals(2, sqlParameterSourceAwards.length);
+		    IntStream.range(0, 2).forEach(i -> assertAwardSource(sqlParameterSourceAwards, i, 10 ));
+		
 
+	}
 }
